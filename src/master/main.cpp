99a8c9af0ec2082fc866168cc481897631fdda10
Montanarino <supermasupersuper1234@gmail.com>
2026-03-07 22:54:40 +0100

    fixes

--- src/master/main.cpp
+++ src/master/main.cpp
@@ -2,24 +2,26 @@
 #include "LoRaModule.cpp"
 
 // put function declarations here:
-LoRaModule lora(5, 6, 9600);
+LoRaModule* lora;
 void loraSetup();
 void onTx();
 void onRx(uint8_t*,uint16_t);
 void onRxTimeout();
 const uint8_t dataExpected[] = {'c', 'i', 'a', 'o'};
+const uint8_t data[] = {'c', 'i', 'a', 'o'};
 
 void setup() {
-
-  lora.onTxDone(onTx);
-  lora.onRxDone(onRx);
-  lora.onRxTimeout(onRxTimeout);
-  lora.setExpect(dataExpected, 5);
+  lora = new LoRaModule(4, 5, 9600);
+  lora->onTxDone(onTx);
+  lora->onRxDone(onRx);
+  lora->onRxTimeout(onRxTimeout);
+  lora->setExpect(dataExpected, 5);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lora.poll();
+  lora->poll();
 }
 
 // put function definitions here:
@@ -27,16 +29,16 @@ void loraSetup(){
 
 }
 void onTx(){
-  printf("Inviato con successo, inizio ascolto...\n");
-  lora.startReceive(1000);
+  Serial.print("Inviato con successo, inizio ascolto...\n");
+  lora->startReceive(1000);
 }
 void onRx(uint8_t* d,uint16_t l){
-  printf("Ricevuto");
+  Serial.print("Ricevuto: ");
   for(int i = 0; i < l; i++){
-    printf("%c", d[i]);
+    Serial.print(d[i]);
   }
-  printf("\n");
+  Serial.print("\n");
 }
 void onRxTimeout(){
-  
+  //lora.send(data, sizeof(data))
 }
